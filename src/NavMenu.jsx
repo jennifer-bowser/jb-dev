7548e15295641145b69c2f241a71b4827ae71026
Jennifer Bowser <jduffbowser@gmail.com>
2024-05-30 14:42:24 -0700

    makes NavMenu stateful

--- src/NavMenu.jsx
+++ src/NavMenu.jsx
@@ -1,28 +1,33 @@
 import "./NavMenu.css";
+import { useState } from "react";
 
 export default function NavMenu(){
+    const sections = [
+        "home",
+        "portfolio",
+        "about",
+        "resume",
+        "contact"
+    ]
+    const [activeSection, setActiveSection] = useState(sections[0]);
+
+    const isActive = (section) => {
+        console.log(`${section} is active: ${activeSection === section}`)
+        return activeSection === section;
+    }
+
+    const handleClick = (section) => {
+        setActiveSection(section);
+    }
+
     return (
         <ul className="NavMenu">
-            <li>
-                <a href="#">home</a>
-                <div className="NavMenu-Highlight Active"></div>
-            </li>
-            <li>
-                <a href="#">portfolio</a>
-                <div className="NavMenu-Highlight"></div>
-            </li>
-            <li>
-                <a href="#">about</a>
-                <div className="NavMenu-Highlight"></div>
-            </li>
-            <li>
-                <a href="#">contact</a>
-                <div className="NavMenu-Highlight"></div>
-            </li>
-            <li>
-                <a href="#">resume</a>
-                <div className="NavMenu-Highlight"></div>
-            </li>
+            {sections.map( (section, index) => (
+                <li key={index}>
+                    <a href={`#${section}`} onClick={() => handleClick(section)}>{section}</a>
+                    <div className={`NavMenu-Underline${isActive(section) ? " NavMenu-Underline-Active" : ""}`}></div>
+                </li>
+            ))}
         </ul>
     );
 }
