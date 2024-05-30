7bee9104e64027dd9e34d49867226396d3e8b6d9
Jennifer Bowser <jduffbowser@gmail.com>
2024-05-30 11:41:50 -0700

    creates 'NavMenu' component

---
+++ src/NavMenu.jsx
@@ -0,0 +1,13 @@
+import "./NavMenu.css";
+
+export default function NavMenu(){
+    return (
+        <ul className="NavMenu">
+            <li>home</li>
+            <li>portfolio</li>
+            <li>about</li>
+            <li>contact</li>
+            <li>resume</li>
+        </ul>
+    );
+}
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,16 +1,11 @@
+import NavMenu from "./NavMenu"
 import "./Navbar.css"
 
 export default function Navbar(){
     return (
         <div className="Navbar">
             <div>Logo goes here</div>
-            <ul className="Navbar-Ul">
-                <li>home</li>
-                <li>portfolio</li>
-                <li>about</li>
-                <li>contact</li>
-                <li>resume</li>
-            </ul>
+            <NavMenu />
         </div>
     )
 }
