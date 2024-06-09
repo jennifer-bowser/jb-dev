a22ffcf068bc5b463b26ede5fcc6ec314ff82219
jennifer.bowser <jduffbowser@gmail.com>
2024-06-09 10:38:23 -0700

    a better way to do the Separator

--- src/Separator.jsx
+++ src/Separator.jsx
@@ -1,9 +1,10 @@
 import "./Separator.css";
 
-export default function Separator(){
+export default function Separator() {
     return (
         <div className="Separator">
             <a className="Separator-Link" href="#portfolio"> <span className="Accent-Text">&larr;</span> have a look</a>
+            <div className="Separator-Line"></div>
         </div>
     )
 }
--- src/TypingLetters.jsx
+++ src/TypingLetters.jsx
@@ -1,8 +1,8 @@
 import "./TypingLetters.css";
 
-export default function TypingLetters({text}){
+export default function TypingLetters({ text }) {
     return (
-        <div>
+        <div className="TypingLetters">
             <p className="TypingLetters-Text">{text}</p>
             <div className="TypingLetters-Background"></div>
         </div>
