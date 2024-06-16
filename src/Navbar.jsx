1e6c0ef675fbfb424783d1a969d839602b1fbadf
jennifer.bowser <jduffbowser@gmail.com>
2024-06-16 13:59:37 -0700

    moves jb-dev logos into their own subfolder

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,11 @@
 import NavMenu from "./NavMenu"
 import "./Navbar.css"
 
-export default function Navbar({sectionTitles}){
+export default function Navbar({ sectionTitles }) {
     return (
         <div className="Navbar">
-            <a href="#home" className="Navbar-Logolink"><img src="src/assets/jb-logo-full.svg" width="275px"/></a>
-            <NavMenu sectionTitles={sectionTitles}/>
+            <a href="#home" className="Navbar-Logolink"><img src="src/assets/jb-dev-logos/jb-logo-full.svg" width="275px" /></a>
+            <NavMenu sectionTitles={sectionTitles} />
         </div>
     )
 }
