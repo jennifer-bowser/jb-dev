0ed34260706bf7e0d0c538761a714ce507d528ed
jennifer.bowser <jduffbowser@gmail.com>
2024-06-16 14:06:04 -0700

    working on heading animations

--- src/Section.jsx
+++ src/Section.jsx
@@ -2,53 +2,56 @@ import About from "./About";
 import Resume from "./Resume";
 import { titleCase } from "./Util";
 import "./Section.css";
-// import { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 
-export default function Section({title}){
-    // const headingRef = useRef(null);
+export default function Section({ title }) {
+    const headingRef = useRef(null);
 
-    // const observerFunction = (entries) => {
-    //     const [entry] = entries;
+    const observerFunction = (entries) => {
+        const [entry] = entries;
 
-    //     const classList = entry.target.classList;
-    //     const animationClassName = "Section-Heading-Animation";
+        const classList = entry.target.classList;
+        const animationClassName = "Section-Heading-Animation";
 
-    //     // entry.isIntersecting
-    //     //     ? entry.target.classList.add("Section-Heading-Animation")
-    //     //     : entry.target.classList.remove("Section-Heading-Animation");
+        // entry.isIntersecting
+        //     ? entry.target.classList.add("Section-Heading-Animation")
+        //     : entry.target.classList.remove("Section-Heading-Animation");
 
-    //     // entry.target.classList.toggle("Section-Heading-Animation");
-    //     if(entry.isIntersecting && !classList.contains(animationClassName)){
-    //         classList.add(animationClassName);
-    //     }
-    //     else{
-    //         classList.remove(animationClassName);
-    //     }
-    // }
+        // entry.target.classList.toggle("Section-Heading-Animation");
+        if (entry.isIntersecting && !classList.contains(animationClassName)) {
+            classList.add(animationClassName);
+        }
+        else {
+            classList.remove(animationClassName);
+        }
+    }
 
-    // const options = {
-    //     root: null,
-    //     rootMargin: "0px",
-    //     threshold: 1.0
-    // }
 
-    // useEffect( () => {
-    //     const observer = new IntersectionObserver(observerFunction, options);
-    //     if(headingRef.current) observer.observe(headingRef.current);
+    useEffect(() => {
+        const options = {
+            root: null,
+            rootMargin: "0px",
+            threshold: 1.0
+        }
 
-    //     return () => {
-    //         if(headingRef.current) observer.unobserve(headingRef.current);
-    //     }
-    // }, [headingRef, options])
+        const observer = new IntersectionObserver(observerFunction, options);
+        const headingRefCurrent = headingRef.current;
+
+        if (headingRefCurrent) observer.observe(headingRefCurrent);
+
+        return () => {
+            if (headingRefCurrent) observer.unobserve(headingRefCurrent);
+        }
+    }, [headingRef])
 
     const components = {
         "about": <About />,
         "resume": <Resume />
     }
 
-    return(
+    return (
         <section className={`Section ${titleCase(title)}`} id={title}>
-            <h1 /*ref={headingRef}*/>{title}<span className="Accent-Text">.</span></h1>
+            <h1 ref={headingRef}>{title}<span className="Accent-Text">.</span></h1>
             {components[title] ?? ""}
         </section>
     )
