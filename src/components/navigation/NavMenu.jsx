e8d26b6511c6e0947173ec9be636e4ecb9a2441d
Jennifer Bowser <jduffbowser@gmail.com>
2024-06-19 15:05:53 -0700

    updates NavMenu link behavior on hover (no need for 'active' state anymore)

--- src/components/navigation/NavMenu.jsx
+++ src/components/navigation/NavMenu.jsx
@@ -1,21 +1,12 @@
 import "./NavMenu.css";
-import { useState } from "react";
 
 export default function NavMenu({sections}){
-    const [activeSection, setActiveSection] = useState(sections[0]);
-
-    const isActive = (section) => activeSection === section;
-
-    const handleClick = (section) => {
-        setActiveSection(section);
-    }
-
     return (
         <ul className="NavMenu">
             {sections.map( (section, index) => (
                 <li key={index}>
                     <a href={`#${section}`} onClick={() => handleClick(section)}>{section}</a>
-                    <div className={`NavMenu-Underline${isActive(section) ? " NavMenu-Underline-Active" : ""}`}></div>
+                    <div className="NavMenu-Underline"></div>
                 </li>
             ))}
         </ul>
