ddcad951671712e8c73135f801fb2a506dcf6b3f
Jennifer Bowser <jduffbowser@gmail.com>
2024-06-19 15:00:33 -0700

    finishes Back To Top button

--- src/components/navigation/BackToTop.jsx
+++ src/components/navigation/BackToTop.jsx
@@ -1,10 +1,10 @@
 import "./BackToTop.css";
-// import upArrowIcon from "../../assets/icons/up-arrow-icon.svg";
+import upArrowIcon from "../../assets/icons/up-arrow-icon.svg";
 
 export default function BackToTop(){
     return (
-        <a className="BackToTop" aria-label="Back To Top">
-            {/* <img src={upArrowIcon} alt="Back To Top"></img> */}
+        <a className="BackToTop" href="#home" aria-label="Back To Top">
+            <img src={upArrowIcon} alt="Back To Top"></img>
         </a>
     );
 }
