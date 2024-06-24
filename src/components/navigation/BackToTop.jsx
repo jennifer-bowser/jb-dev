2a101720beae9460c39dc4e649ddf908bb4f35a1
Jennifer Bowser <jduffbowser@gmail.com>
2024-06-24 11:54:19 -0700

    hides BackToTop button when already at top

--- src/components/navigation/BackToTop.jsx
+++ src/components/navigation/BackToTop.jsx
@@ -1,9 +1,31 @@
 import "./BackToTop.css";
 import upArrowIcon from "../../assets/icons/up-arrow-icon.svg";
+import { useEffect, useState } from "react";
 
 export default function BackToTop(){
+    // used to hide BackToTop button when user is already at the top of the site
+    const SCROLL_HEIGHT = 300;
+    const [showButton, setShowButton] = useState(window.scrollY >= SCROLL_HEIGHT);
+
+    useEffect( () => {
+        function handleScroll() {
+            if(window.scrollY >= SCROLL_HEIGHT && !showButton){
+                setShowButton(true);
+            }
+            else if(window.scrollY < SCROLL_HEIGHT && showButton){
+                setShowButton(false);
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll)
+
+        return _ => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    });
+
     return (
-        <a className="BackToTop" href="#home" aria-label="Back To Top">
+        <a className={"BackToTop" + (showButton ? " BackToTop-Visible" : " BackToTop-Invisible")} href="#home" aria-label="Back To Top">
             <img src={upArrowIcon} alt="Back To Top"></img>
         </a>
     );
