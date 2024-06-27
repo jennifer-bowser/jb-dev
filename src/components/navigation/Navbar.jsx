5f46289d4dc8e72072f5eca68e596992e45ae56b
Jennifer Bowser <jduffbowser@gmail.com>
2024-06-27 12:16:14 -0700

    changes Navbar responsive design

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,34 +1,12 @@
 import NavMenu from "./NavMenu"
 import fullLogo from "../../assets/jb-dev-logos/jb-logo-full.svg";
-import miniLogo from "../../assets/jb-dev-logos/jb-logo-mini.svg";
-import { useState, useEffect } from "react";
 import "./Navbar.css"
 
 export default function Navbar({ sections }) {
-    // used to switch to miniLogo if window gets too small
-    const MIN_WIDTH = 750;
-    const [useMiniLogo, setUseMiniLogo] = useState(window.innerWidth <= MIN_WIDTH);
-
-    useEffect( () => {
-        function handleResize() {
-            if(window.innerWidth <= MIN_WIDTH && !useMiniLogo){
-                setUseMiniLogo(true);
-            }
-            else if(window.innerWidth > MIN_WIDTH & useMiniLogo){
-                setUseMiniLogo(false);
-            }
-        }
-
-        window.addEventListener('resize', handleResize)
-
-        return _ => {
-            window.removeEventListener('resize', handleResize)
-        }
-    });
-
     return (
         <div className="Navbar">
-            <img src={useMiniLogo ? miniLogo : fullLogo} className={useMiniLogo ? "MiniLogo" : "FullLogo"} height="40"/>
+            <img src={fullLogo} height="40" className="Navbar-FullLogo"/>
+            <div className="Navbar-Spacer"></div>
             <NavMenu sections={sections} />
         </div>
     )
