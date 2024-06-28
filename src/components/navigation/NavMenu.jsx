09552386960e6ecf77d4818ee7f52156f8cbc350
Jennifer Bowser <jduffbowser@gmail.com>
2024-06-28 11:40:38 -0700

    WIP - getting mobile nav to work

--- src/components/navigation/NavMenu.jsx
+++ src/components/navigation/NavMenu.jsx
@@ -1,14 +1,59 @@
 import "./NavMenu.css";
+import menuIcon from "../../assets/icons/menuIcon.svg";
+import { useEffect, useState } from "react";
 
 export default function NavMenu({sections}){
+    /*--  Toggle whether full menu vs menu button is visible --*/
+    const MIN_WIDTH = 750;
+    const [isNarrowWidth, setIsNarrowWidth] = useState(window.innerWidth <= MIN_WIDTH);
+
+    useEffect( () => {
+        function handleResize() {
+            if(window.innerWidth <= MIN_WIDTH && !isNarrowWidth){
+                setIsNarrowWidth(true);
+                setShowNavMenu(false);
+            }
+            else if(window.innerWidth > MIN_WIDTH & isNarrowWidth){
+                setIsNarrowWidth(false);
+                setShowNavMenu(true);
+            }
+        }
+
+        window.addEventListener('resize', handleResize)
+
+        return _ => {
+            window.removeEventListener('resize', handleResize)
+        }
+    });
+
+    /*-- Handles showing/hiding the NavMenu vs menu button --*/
+    const [showNavMenu, setShowNavMenu] = useState(!isNarrowWidth);
+
+    const handleMenuButtonClick = () => {
+        setShowNavMenu(true);
+    }
+
+    /* 
+        case 1: NOT narrow width
+            > Show full menu
+        
+        case 2: Narrow width, menu button NOT clicked
+            > Show menu icon
+
+        case 3: Narrow width, menu button IS clicked
+            > Show popout menu
+     */
     return (
-        <ul className="NavMenu">
-            {sections.map( (section, index) => (
-                <li key={index}>
-                    <a href={`#${section}`} onClick={() => handleClick(section)}>{section}</a>
-                    <div className="NavMenu-Underline"></div>
-                </li>
-            ))}
-        </ul>
+        <>
+            <img src={menuIcon} className={"MenuIcon " + (isNarrowWidth ? "Visible" : "Invisible")} onClick={handleMenuButtonClick} width="40"/>
+            <ul className={"NavMenu" + (isNarrowWidth ? " Popout" : " Full") + (showNavMenu ? " Visible" : " Invisible")}>
+                {sections.map( (section, index) => (
+                    <li key={index}>
+                        <a href={`#${section}`}>{section}</a>
+                        <div className="NavMenu-Underline"></div>
+                    </li>
+                ))}
+            </ul>
+        </>
     );
 }
--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -3,11 +3,12 @@ import fullLogo from "../../assets/jb-dev-logos/jb-logo-full.svg";
 import "./Navbar.css"
 
 export default function Navbar({ sections }) {
+
+    
     return (
         <div className="Navbar">
             <img src={fullLogo} height="40" className="Navbar-FullLogo"/>
-            <div className="Navbar-Spacer"></div>
-            <NavMenu sections={sections} />
+            <NavMenu sections={sections}/>
         </div>
     )
 }
