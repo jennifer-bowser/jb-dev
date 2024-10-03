00508c4442290eab99356438e8d9745cae6f6e08
jennifer.bowser <jduffbowser@gmail.com>
2024-10-02 20:12:45 -0700

    updates resume text

--- src/components/sections/Resume/Resume.jsx
+++ src/components/sections/Resume/Resume.jsx
@@ -4,20 +4,59 @@ import sidwaysArrow from "../../../assets/flourishes/sideways-arrow.svg";
 import openNewTabIcon from "../../../assets/icons/open-new-tab-icon.svg"
 
 export default function Resume() {
-    const workExperienceListItems = [
-        "Develop and mantain full-stack features for data visualization web app using PHP and Javascript/React to provide insights needed to build and develop education institutions, companies, and communities.",
-        "Collaborate with cross-functional team members to design new features and to incorporate new data streams into existing tools.",
-        "Communicate with Product Managers to identify minimum viable product requirements and clearly defined feature sets.",
-        "Conduct regular code reviews and offer constructive feedback to maintain best-practice standards throughout the codebase.",
-        "Engage in Agile development processes, including sprint planning, daily stand-up, and retrospectives."
+    const responsabilities = [
+        "Design and build full-stack, interactive, reusable features for Lightcast’s flagship application using JavaScript (React, D3.js) and PHP.",
+        "Maintain and optimize labor analytics software by identifying and resolving bugs, implementing feature updates, and optimizing code performance, helping to create a seamless experience for over 30,000 enterprise customers.",
+        "Developed robust interfaces to consume and display data from RESTful APIs and relational SQL databases (MySQL, PostgreSQL), optimizing data retrieval and processing for enhanced performance.",
+        "Architect technical strategies to translate feature requests into scalable, robust, and easily-maintained solutions, enabling the development of hundreds of new and upgraded features.",
+        "Utilize GitLab for CI/CD pipelines to automate build, test, and deployment processes, ensuring rapid and efficient code integration and delivery.",
+        "Responsible for key aspects of Agile Scrum development processes using Jira and Confluence, including sprint planning and daily stand-ups"
     ];
 
-    const skillsListItems = [
-        <><span className="Bold">Technologies: </span>JavaScript, Node.js, PHP, Java, Python, SQL, MongoDB, D3.js, React, APIs, Express,js, FXML, XML, HTML, CSS</>,
-        <><span className="Bold">Tools: </span>Git, GitHub, GitLab, Jira, Adobe Photoshop, Adobe Illustrator, Figma, Invision</>
-    ];
+    const accomplishments = [
+        "Served as lead engineer on a cross-team project to integrate new data streams into a legacy product, enabling the development of new and innovative features, directly driving over $1.2M in sales within 6 months of the product refresh.",
+        "Drove the re-architecture of our application’s data consumption for fundamental data types, applying SOLID design principles and patterns to enhance system scalability and maintainability.",
+        "Spearheaded codebase modernization by upgrading our back-end language to the latest version, enhancing performance and maintainability while ensuring alignment with best practices."
+    ]
+
+    const langsAndFrameworks = [
+        "JavaScript",
+        "React",
+        "Java",
+        "Python",
+        "Node.js",
+        "SQL (MySQL & PostgreSQL)",
+        "MongoDB",
+        "REST APIs",
+        "PHP",
+        "Kotlin",
+        "Android SDK",
+        "D3.js",
+        "Express.js",
+        "FXML",
+        "XML",
+        "HTML",
+        "SASS/CSS"
+    ]
+
+    const toolsAndTech = [
+        "Git",
+        "GitHub",
+        "GitLab",
+        "Jira",
+        "Confluence",
+        "IntelliJ",
+        "Visual Studio Code",
+        "Visual Studio",
+        "Android Studio",
+        "Figma",
+        "InVision",
+        "Adobe Photoshop",
+        "Adobe Illustrator"
+    ]
 
     const certListItems = [
+        "AWS Certified Cloud Practitioner",
         "CompTIA A+ Certification",
         "CompTIA Project+ Certification",
         "Google IT Certification"
@@ -37,12 +76,13 @@ export default function Resume() {
                         <div className="Resume-Heading-Block-Expander"></div>
                     </div>
                     <h4>Full-Stack Software Engineer II&nbsp;&nbsp;—&nbsp;&nbsp;Lightcast</h4>
-                    <p className="Subtitle">Moscow, ID | March 2023 — Present</p>
-                    <BulletList listItems={workExperienceListItems} />
-                    <div>
-                        <h4>Full-Stack Software Engineer I&nbsp;&nbsp;—&nbsp;&nbsp;Lightcast</h4>
-                        <p className="Subtitle">Moscow, ID | May 2022 — March 2023</p>
-                    </div>
+                    <p className="Subtitle">March 2023 — Present</p>
+                    <h4>Full-Stack Software Engineer I&nbsp;&nbsp;—&nbsp;&nbsp;Lightcast</h4>
+                    <p className="Subtitle">May 2022 — March 2023</p>
+                    <h5>Responsabilities:</h5>
+                    <BulletList listItems={responsabilities} />
+                    <h5>Key Accomplishments:</h5>
+                    <BulletList listItems={accomplishments} />
                 </section>
                 <section>
                     <div className="Resume-Heading-Block">
@@ -60,10 +100,6 @@ export default function Resume() {
                         <h4>B.S. Software Development&nbsp;&nbsp;—&nbsp;&nbsp; Western Governors University</h4>
                         <p className="Subtitle">Seattle, WA | April 2022</p>
                     </section>
-                    <section>
-                        <h4>B.A. Biology&nbsp;&nbsp;—&nbsp;&nbsp;Pepperdine University</h4>
-                        <p className="Subtitle">Malibu, CA | May 2017</p>
-                    </section>
                 </section>
                 <section className="Resume-Skills-Section">
                     <div className="Resume-Heading-Block">
@@ -73,7 +109,10 @@ export default function Resume() {
                         </div>
                         <div className="Resume-Heading-Block-Expander"></div>
                     </div>
-                    <BulletList listItems={skillsListItems} />
+                    <h4>Languages & Frameworks</h4>
+                    <BulletList listItems={langsAndFrameworks} />
+                    <h4>Tools & Technologies</h4>
+                    <BulletList listItems={toolsAndTech} />
                 </section>
                 <section>
                     <div className="Resume-Heading-Block">
